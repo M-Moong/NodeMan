c5b478344586ca23df50a76ebd2a8eeb0f6798c8
Moong <tmdals9154@gmail.com>
2023-03-10 00:36:15 +0900

    node2 - mongoDB에 값 저장하기

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 
 const MongoClient = require('mongodb').MongoClient;
+
+var db;
 MongoClient.connect('mongodb+srv://admin:1234@cluster0.mhl7op5.mongodb.net/?retryWrites=true&w=majority', function(에러, client) {
+    //연결되면 할일
+    if(에러) return console.log(에러)
+    db = client.db('todoapp');  // todoapp이라는 database에 연결중요
 
-app.listen(8080, function(){
-    console.log('listening on 8080');
+    db.collection('post').insertOne( {이름 : 'John', 나이 : 20, _id : 100} , function(에러, 결과){
+        console.log('저장완료');
+    });
+
+    app.listen(8080, function(){
+      console.log('listening on 8080');
+    });
 });
-
-})
 
 
 
@@ -40,7 +48,14 @@ app.post('/add', function(request, response){
     console.log(request.body.title);
     console.log(request.body.date);
 
+    db.collection('post').insertOne( {제목 : request.body.title, 날짜 : request.body.date} , function(에러, 결과){
+        console.log('(제목, 날짜) 저장완료');
+    });
+
     // DB에 저장해주세요
-    
 })
+
+// 어떤 사람이 /add 라는 경로로 post 요청을 하면,
+// 데이터 2개 (날짜, 제목)을 보내주는데,
+// 이때, 'post'라는 이름을 가진 collection에 두개 데이터를 저장하기
 
